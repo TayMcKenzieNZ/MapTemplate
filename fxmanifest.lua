a85a825c63649ad0214c5ec244c8799d3a0f6f69
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-10-17 14:04:09 +1300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+
+
+--- If the map includes custom props, you will need to add the ytyp name below for them to appear in your FiveM server ---
+--- Simply uncomment the lines below and replace the quoted ytyp. ---
+
+
+-- data_file "DLC_ITYP_REQUEST" "ytyp_name_here.ytyp"
+
